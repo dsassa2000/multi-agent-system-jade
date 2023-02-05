259ad6fed5f1cafa942e7b28a53347e435f70b98
doha <doha.sassa@uit.ac.ma>
2023-02-05 17:36:00 +0100

    AgentTow class : call searcher and indexer

--- src/de/unihildesheim/iis/jadedemo/AgentTwo.java
+++ src/de/unihildesheim/iis/jadedemo/AgentTwo.java
@@ -1,16 +1,55 @@
 package de.unihildesheim.iis.jadedemo;
 
 import jade.core.Agent;
+
 import jade.core.behaviours.CyclicBehaviour;
 import jade.lang.acl.ACLMessage;
 
-/**
- * @author Viktor Eisenstadt
- */
+import java.io.IOException;
+
+import org.apache.lucene.document.Document;
+import org.apache.lucene.queryParser.ParseException;
+import org.apache.lucene.search.ScoreDoc;
+import org.apache.lucene.search.TopDocs;
+
 public class AgentTwo extends Agent {
   private static final long serialVersionUID = 1L;
+  
+  String indexDir = "C:\\Users\\HP\\Documents\\INDEX";                        /*c:\\Lucene\\Index";*/
+  String dataDir =   "C:\\Users\\HP\\Documents\\JAVA\\1. Course Introduction";                                                       /*"c:\\Lucene\\Data";*/
+  indexer indexer;
+  searcher searcher;
+  
+  // create indexing
+  private void createIndex() throws IOException {
+      indexer = new indexer(indexDir);
+      int numIndexed;
+      long startTime = System.currentTimeMillis();	
+      numIndexed = indexer.createIndex(dataDir, new textFileFilter());
+      long endTime = System.currentTimeMillis();
+      indexer.close();
+      System.out.println(numIndexed+" File indexed, time taken: "
+         +(endTime-startTime)+" ms");		
+   }
+  // searching 
+  private void search(String searchQuery) throws IOException, ParseException {
+      searcher = new searcher(indexDir);
+      long startTime = System.currentTimeMillis();
+      TopDocs hits = searcher.search(searchQuery);
+      long endTime = System.currentTimeMillis();
+   
+      System.out.println(hits.totalHits +
+         " documents found. Time :" + (endTime - startTime));
+      for(ScoreDoc scoreDoc : hits.scoreDocs) {
+         Document doc = searcher.getDocument(scoreDoc);
+            System.out.println("File: "
+            + doc.get(LuceneConstants.FILE_PATH));
+      }
+      searcher.close();
+   }
 
-  protected void setup() {
+
+  public void setup() {
 
     // Define the behaviour
     CyclicBehaviour loop = new CyclicBehaviour(this) {
@@ -18,16 +57,26 @@ public class AgentTwo extends Agent {
 
       @Override
       public void action() {
-
+          
         // Receive the incoming message
         ACLMessage aclMsg = receive();
-
+          
         // Interpret the message
         if (aclMsg != null) {
           System.out.println(myAgent.getLocalName()
               + "> Received message from: " + aclMsg.getSender());
           System.out.println("Message content: " + aclMsg.getContent());
           // TODO Aufgabe 1
+          try {
+        	  createIndex();
+        	  search(aclMsg.getContent());
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}catch (ParseException e) {
+	         e.printStackTrace();
+	     }
+          // searching word
         }
         block(); // Stop the behaviour until next message is received
       }
