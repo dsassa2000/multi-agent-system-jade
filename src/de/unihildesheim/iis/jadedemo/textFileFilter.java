382c473ac8218c40aed661a9cda1548e48dd4e54
doha <doha.sassa@uit.ac.ma>
2023-02-06 20:47:08 +0100

    TextFileFilter : add all extention

--- src/de/unihildesheim/iis/jadedemo/textFileFilter.java
+++ src/de/unihildesheim/iis/jadedemo/textFileFilter.java
@@ -6,8 +6,20 @@ import java.io.FileFilter;
 public class textFileFilter implements FileFilter{
 
 	@Override
-	   public boolean accept(File pathname) {
-	      return pathname.getName().toLowerCase().endsWith(".txt");
-	   }
+	public boolean accept(File pathname) {
+
+		if (!pathname.exists()) {
+		    System.out.println(pathname + " does not exist.");
+		}
+		    String filename = pathname.getName().toLowerCase();
+		    if(filename.endsWith(".txt"))return filename.endsWith(".txt");
+		    if(filename.endsWith(".html"))return filename.endsWith(".html");
+		    if(filename.endsWith(".xml"))return filename.endsWith(".xml");
+		    if(filename.endsWith(".pdf"))return filename.endsWith(".pdf");
+		    else {
+			System.out.println("Skipped " + filename);
+		    }
+		    return false;
+	}
 	
 }
